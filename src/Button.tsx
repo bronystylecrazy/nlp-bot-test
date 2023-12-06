0048ff5991fa0811c0da3a7a7d372d5d3fc76d17
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2023-12-07 04:32:15 +0700

    Update Button.tsx

--- src/Button.tsx
+++ src/Button.tsx
@@ -8,12 +8,10 @@ export default function TestButton(props: ButtonProps) {
   const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
-    document.title = `You clicked ${count} times 5555`;
+    document.title = `You clicked ${count} times`;
   });
 
   return (
-    <button onClick={() => setCount((c) => c + 5555)}>
-      Click Me {count} time.
-    </button>
+    <button onClick={() => setCount(c + 1)}>Click Me {count} time....</button>
   );
 }
