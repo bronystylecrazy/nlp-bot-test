4c78467454483af4c7f8b577d5a86eec5bf98d07
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2023-12-07 04:19:23 +0700

    Update Button.tsx

--- src/Button.tsx
+++ src/Button.tsx
@@ -11,5 +11,7 @@ export default function TestButton(props: ButtonProps) {
     document.title = `You clicked ${count} times`;
   });
 
-  return <button onClick={() => setCount(c + 1)}>Click Me {count}</button>;
+  return (
+    <button onClick={() => setCount(c + 1)}>Click Me {count} time. </button>
+  );
 }
